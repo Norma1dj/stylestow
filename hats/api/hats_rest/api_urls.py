252944612b750c5dbbd38cfe87277e3241ee4969
Long G <long.w.guan@gmail.com>
2023-07-19 13:38:39 -0700

    "Added post method for hats_rest"

--- hats/api/hats_rest/api_urls.py
+++ hats/api/hats_rest/api_urls.py
@@ -3,4 +3,5 @@ from .api_views import api_list_hat
 
 urlpatterns = [
     path("hats/", api_list_hat, name="api_list_hat"),
+    path("hats/<int:pk>/", api_list_hat, name="api_show_hat"),
 ]
--- hats/api/hats_rest/api_views.py
+++ hats/api/hats_rest/api_views.py
@@ -2,6 +2,7 @@ from .models import Hat
 from django.views.decorators.http import require_http_methods
 from common.json import ModelEncoder
 from django.http import JsonResponse
+import json
 
 class HatListEncoder(ModelEncoder):
     model = Hat
@@ -13,11 +14,22 @@ class HatListEncoder(ModelEncoder):
     ]
 
 
-@require_http_methods(["GET"])
-def api_list_hat(request):
+@require_http_methods(["GET", "POST"])
+def api_list_hat(request, pk=None):
     if request.method == "GET":
-        hats = Hat.objects.all()
-    return JsonResponse(
-        {"hats" : hats},
-        encoder = HatListEncoder,
-    )
+        if pk == None:
+            hats = Hat.objects.all()
+        else:
+            hats = Hat.objects.filter(id=pk)
+        return JsonResponse(
+            {"hats" : hats},
+            encoder = HatListEncoder,
+        )
+    else:
+        content = json.loads(request.body)
+        hat = Hat.objects.create(**content)
+        return JsonResponse(
+            hat,
+            encoder= HatListEncoder,
+            safe=False,
+        )
