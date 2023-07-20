850bb3cafafe049180ce9b886a8783feb230fbd5
Drew Norman-Meadows <norma1dj@gmail.com>
2023-07-20 15:20:27 -0700

    resolved commits

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,11 +3,8 @@ import MainPage from './MainPage';
 import Nav from './Nav';
 import HatList from './HatList';
 import ShoeList from './ShoeList';
-<<<<<<< HEAD
 import CreateShoeForm from './CreateShoeForm';
-=======
 import HatForm from './HatForm';
->>>>>>> dae230be18d8af7b4a305f6b0f6c06b8e5721cee
 
 function App() {
   return (
@@ -18,13 +15,10 @@ function App() {
           <Route path="/" element={<MainPage />} />
           <Route path="/hats" element={<HatList />} />
           <Route path="/shoes" element={<ShoeList />} />
-<<<<<<< HEAD
           
           <Route path="/shoes/create" element={<CreateShoeForm />} />
 
-=======
           <Route path="/hats/new" element={<HatForm />} />
->>>>>>> dae230be18d8af7b4a305f6b0f6c06b8e5721cee
         </Routes>
       </div>
     </BrowserRouter>
