9012c18ac6359b2761c003b472624d4d0e6a17a1
Drew Norman-Meadows <norma1dj@gmail.com>
2023-09-29 12:40:00 -0700

    Removed home link in navbar

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,9 +10,9 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+            {/* <li className="nav-item">
               <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
-            </li>
+            </li> */}
             <li className="nav-item">
               <NavLink className="nav-link" to="/shoes">Shoes</NavLink>
             </li>
